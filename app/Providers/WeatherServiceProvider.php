d2c10f48f8e540e4660a8d8021d59181cebf62fa
Chris Reinschlid <crreinschild@gmail.com>
2025-03-28 22:43:57 -0400

    added a placeholder weather service

--- app/Providers/WeatherServiceProvider.php
+++ app/Providers/WeatherServiceProvider.php
@@ -1,11 +1,16 @@
 <?php
 
+namespace App\Providers;
+
+use App\Services\WeatherService;
+use Illuminate\Support\ServiceProvider;
+
 class WeatherServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->singleton('weather', function ($app) {
-            return new Weather(); // Example temperature
+            return new WeatherService(); // Example temperature
         });
     }
 }
