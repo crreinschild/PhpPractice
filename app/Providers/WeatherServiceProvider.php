ba7acd3e76c67532b921364ebe639016058aaeb6
Chris Reinschlid <crreinschild@gmail.com>
2025-03-30 11:10:19 -0400

    documentation of WeatherService and WeatherServiceProvider

--- app/Providers/WeatherServiceProvider.php
+++ app/Providers/WeatherServiceProvider.php
@@ -5,8 +5,19 @@ namespace App\Providers;
 use App\Services\WeatherService;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * WeatherServiceProvider
+ *
+ * Adds the WeatherService to the service container.
+ *
+ * @package App\Providers
+ */
 class WeatherServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the WeatherService.
+     * I supposed it doesn't need to be a singleton.
+     */
     public function register()
     {
         $this->app->singleton('weather', function ($app) {
